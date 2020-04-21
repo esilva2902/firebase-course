976ba655593a34bfba53a4d19f1900b0aef29345
Erick Morales Silva <erick.silva@localhost.localdomain>
2020-04-20 20:29:53 -0500

    v0.0.6 - firebaseui-web was added to login component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,9 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+    /**
+     * 2. This is the FirebaseUI reference:
+     */
     ui: firebaseui.auth.AuthUI;
 
     constructor(private afAuth: AngularFireAuth,
@@ -22,6 +25,14 @@ export class LoginComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
 
+        /**
+         * 1. We must configure the library providing the following options:
+         *    (a). signInOptions: these are all the authetication providers our app supports. 
+         *         Remember that these providers must be enabled and configured in Firebase console.
+         * 
+         *    (b). callbacks: We have to provide the functions that is going to be called once the
+         *         user has been signed in.
+         */
         const uiConfig = {
             signInOptions: [
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -36,14 +47,28 @@ export class LoginComponent implements OnInit, OnDestroy {
 
         };
 
+        /**
+         * 3. FirebaseUi constructor needs a global reference to Firebase SDK Authentication service.
+         *    We get the reference from AngularFireAuth service.
+         */
         this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
 
+        /**
+         * 4. Once FirebaseUI is instanciated we can start it passing the following parameters:
+         *    (a). The css selector which correspond to the container where FirebaseUI will set 
+         *         the authentication providers.
+         *    (b). The configuration object with the desirable providers.
+         */
         this.ui.start('#firebaseui-auth-container', uiConfig);
 
 
     }
 
     ngOnDestroy() {
+        /**
+         * 6. We must dispose the FirebaseUi instance because we will get an error saying
+         *    that an intance already exist.
+         */
         this.ui.delete();
     }
 
@@ -51,6 +76,17 @@ export class LoginComponent implements OnInit, OnDestroy {
 
         console.log("Firebase UI result:", result);
 
+        /**
+         * 5. Once the user has successfully logged in we have to redirect to the page we want
+         *    the user starts its interaction. 
+         * 
+         *    Due to our page lives in our current SPA we have to do the redirection inside
+         *    the Angular zone (NgZone) to allowing Angular runs change detection and then display
+         *    the contents of /courses page.
+         * 
+         *    If we were not run the redirection inside the NgZone the content will not be presented 
+         *    to the user.
+         */
         this.ngZone.run(() => this.router.navigateByUrl('/courses'));
 
     }
