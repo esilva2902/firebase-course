4e301480f9b724db48884a091eaac4b97c125af2
Erick Morales Silva <erick.silva@localhost.localdomain>
2020-05-08 15:52:34 -0500

    v0.0.9 - The Angular app was deployed to Firebase Hosting service.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
     production: true,
+    
     firebase: {
-        apiKey: "AIzaSyDg8hs704siPZ_chdtodWYpXicMHxz5nhQ",
-        authDomain: "fir-course-recording.firebaseapp.com",
-        databaseURL: "https://fir-course-recording.firebaseio.com",
-        projectId: "fir-course-recording",
-        storageBucket: "fir-course-recording.appspot.com",
-        messagingSenderId: "706691571098"
+        apiKey: "AIzaSyDYNT7MxddIyM2qUV2tpmAa0ivfJGJ76kY",
+        authDomain: "fir-course-ems.firebaseapp.com",
+        databaseURL: "https://fir-course-ems.firebaseio.com",
+        projectId: "fir-course-ems",
+        storageBucket: "fir-course-ems.appspot.com",
+        messagingSenderId: "1015254468015",
+        appId: "1:1015254468015:web:637a031746c1d746cc5b5a"
     }
 };
